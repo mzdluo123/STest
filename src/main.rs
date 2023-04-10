b01df6e05ec252685f9b64655f45fae77aca817e
RainChan <mzdluo123@foxmail.com>
2023-04-10 17:22:55 +0800

    change url

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ async fn main() {
     let results = tokio::join!(
          test_download("http://dldir1.qq.com/qqfile/qq/QQNT/5333e29d/QQ_v6.9.12-10951.dmg"),
          test_download("http://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v7.0.20.13_29200617_x86.dmg"),
-        test_download("http://issuepcdn.baidupcs.com/issue/netdisk/MACguanjia/4.19.2/BaiduNetdisk_mac_4.19.2_x64.dmg")
+        test_download("http://issuepcdn.baidupcs.com/issue/netdisk/yunguanjia/BaiduNetdisk_7.26.0.10.exe")
     );
     let r = results.0.unwrap() + results.1.unwrap() + results.2.unwrap();
     println!("{:?}", r.show())
@@ -68,12 +68,6 @@ async fn test_download(url: &str) -> SError<TestResult> {
         let start_time = now();
         while let Some(chunk) = rsp.body_mut().data().await {
             let c_len = chunk?.len();
-            if c_len == 0usize {
-                return Ok(TestResult {
-                    len: downloaded,
-                    time: now() - start_time,
-                });
-            }
             downloaded += c_len;
             if now() - start_time > 10_000 { // 最多下载十秒钟
                 return Ok(TestResult {
@@ -82,6 +76,10 @@ async fn test_download(url: &str) -> SError<TestResult> {
                 });
             }
         }
+        return Ok(TestResult {
+            len: downloaded,
+            time: now() - start_time,
+        });
     }
 
     Ok(TestResult {

7adcc9acf018ada007f5204f9f8e0698d96c6b65
RainChan <mzdluo123@foxmail.com>
2023-04-10 18:52:41 +0800

    修改算法

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use std::error::Error;
-use std::ops::Add;
+use std::io::ErrorKind;
+use std::io::ErrorKind::BrokenPipe;
 use chrono::Local;
 use hyper::body::HttpBody;
+use hyper::{Body, Method, Request};
 
 
-type SError<T> = Result<T, Box<dyn Error>>;
+type SError<T> = Result<T, Box<dyn Error + Send + Sync>>;
 
 static UNITS: [&str; 3] = ["B/s", "KB/s", "MB/s"];
+
+fn show_speed(speed_list: &[f32]) -> String {
+    let mut speed: f32 = speed_list.iter().sum();
+    let mut unit = 0;
+    while speed > 1000.0 && unit < UNITS.len() - 1 {
+        speed /= 1024.0;
+        unit += 1;
+    }
+    return format!("{}{}", speed, UNITS[unit]);
+}
 
 #[derive(Debug)]
 struct TestResult {
@@ -17,26 +29,6 @@ struct TestResult {
 impl TestResult {
     fn to_speed(self) -> f32 {
         (self.len as f32) / (self.time as f32) * 1000.0  //每秒钟速度
-    }
-    fn show(self) -> String {
-        let mut speed: f32 = self.to_speed().into();
-        let mut unit = 0;
-        while speed > 1000.0 && unit < UNITS.len() - 1 {
-            speed /= 1024.0;
-            unit += 1;
-        }
-        return format!("{}{}", speed, UNITS[unit]);
-    }
-}
-
-impl Add for TestResult {
-    type Output = TestResult;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self::Output {
-            len: self.len + rhs.len,
-            time: (self.time + rhs.time) / 2,
-        }
     }
 }
 
@@ -54,22 +46,26 @@ async fn main() {
          test_download("http://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v7.0.20.13_29200617_x86.dmg"),
         test_download("http://issuepcdn.baidupcs.com/issue/netdisk/yunguanjia/BaiduNetdisk_7.26.0.10.exe")
     );
-    let r = results.0.unwrap() + results.1.unwrap() + results.2.unwrap();
-    println!("{:?}", r.show())
+    let r = [results.0.unwrap().to_speed(), results.1.unwrap().to_speed(), results.2.unwrap().to_speed()];
+    println!("{}", show_speed(&r))
 }
 
 
 async fn test_download(url: &str) -> SError<TestResult> {
     let mut downloaded = 0usize;
     let client = hyper::Client::new();
-    let url = url.parse()?;
-    let mut rsp = client.get(url).await?;
-    if rsp.status() == 200 {
+    let req = Request::builder()
+        .header("Range", "bytes=0-200000000")
+        .uri(url)
+        .method(Method::GET)
+        .body(Body::empty())?;
+    let mut rsp = client.request(req).await?;
+    if rsp.status() == 206 {
         let start_time = now();
         while let Some(chunk) = rsp.body_mut().data().await {
             let c_len = chunk?.len();
             downloaded += c_len;
-            if now() - start_time > 10_000 { // 最多下载十秒钟
+            if now() - start_time > 5_000 { // 最多下载十秒钟
                 return Ok(TestResult {
                     len: downloaded,
                     time: now() - start_time,
@@ -82,8 +78,5 @@ async fn test_download(url: &str) -> SError<TestResult> {
         });
     }
 
-    Ok(TestResult {
-        len: 0,
-        time: 0,
-    })
+    Err(std::io::Error::from(BrokenPipe).into())
 }
